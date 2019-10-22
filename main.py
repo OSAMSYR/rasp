b7206f4b48f31f4bd5ed6d441ab537eb5434fe76
SHINJUNGWOO <sjo506@naver.com>
2019-10-22 23:11:53 +0900

    bluez2 and test

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class main:
 
         self.rough_data={}
         self.masked_data={}
+        self.state=""
         # key is name and value is list of data
 
         self.blue_handle=blue_handler()
@@ -13,7 +14,7 @@ class main:
 
     def initial_process(self):
         #self.blue_handle.find()
-        self.blue_handle.device_list={"SYR_2":"98:D3:71:FD:7C:04"}
+        self.blue_handle.device_list={"SYR_2":"98:D3:71:FD:7C:04","SYR_3":"98:D3:91:FD:84:13"}
         self.device_addr=self.blue_handle.device_list
         # find complete
 
@@ -26,11 +27,19 @@ class main:
             self.masked_data[name]=self.data_handle.data_masking(self.rough_data.get(name))
             print(self.masked_data)
 
+        self.data_handle.data_min_making(self.masked_data)
+
     def data_save(self):
+        state_string=""
         for name in self.device_addr.keys():
             file_name="./data/"+name+".txt"
+            data_string=""
+            state_string=self.data_handle.state_make(name,self.masked_data.get(name)) + "\n"
+            for val in self.masked_data.get(name):
+                data_string=data_string+" " + val
             with open(file_name, "a+") as f:
-                f.write(str(self.masked_data.get(name)))
+                f.write(data_string)
+        state_string
 
 
 
@@ -39,8 +48,14 @@ class main:
         self.initial_process()
         self.data_processing()
         self.data_save()
-a=main()
-a.start()
+
+
+
+
+
+# test code
+# a=main()
+# a.start()
 
 
         
--- process.py
+++ process.py
@@ -1,17 +1,46 @@
 class data_make:
     def __init__(self):
         self.temp_min=0
-        self.hummid_min=0
         self.light_min=0
+        self.temp_th=0
+        self.light_th=0
+
+
     def data_masking(self,rough):
         tmp="{0:b}".format(int(rough))
         print(tmp)
         emergency=int(tmp[1],2)
         sleep_state=int(tmp[1:3],2)
         tempor=int(tmp[3:10],2)
+        tempor= tempor/4+10
         humid=int(tmp[10:17],2)
         light=int(tmp[17:24],2)
         sound=int(tmp[24:],2)
 
         return [emergency,sleep_state,tempor,humid,light,sound]
+
+    def data_min_making(self, dict):
+        # dict={name:[data]}
+        for name in dict.keys():
+            self.temp_min += dict.get(name)[2]
+            self.light_min+=dict.get(name)[4]
+
+        self.temp_min=self.temp_min/len(dict.keys())
+        self.light_min = self.light_min / len(dict.keys())
+
+    def state_make(self,name,list):
+        #list=[data]
+        state=name
+        if abs(list[2]-self.temp_min)>self.temp_th:
+            list[1] = 3
+        if abs(list[4]-self.light_min)>self.light_min:
+            list[4]= 1 if list[4]-self.light_min >0 else 0
+        for data in list:
+            state= state+" "+data
+        return state
+
+# test code
+# a=data_make()
+# data={"syr1":[0,0,20,40,30,40],"syr2":[0,0,30,20,30,40],"syr3":[0,0,38,20,30,40]}
+# a.data_min_making(data)
 
